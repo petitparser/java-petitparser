26ce93bb91bc6041b3cb8e6b7658a307cccefa2c
Lukas Renggli <renggli@gmail.com>
2012-03-13 00:02:03 +0100

    - fixed an infinite recursion

--- src/org/petitparser/examples/xml/XmlParser.java
+++ src/org/petitparser/examples/xml/XmlParser.java
@@ -1,6 +1,7 @@
 package org.petitparser.examples.xml;
 
 import static org.petitparser.Chars.pattern;
+import static org.petitparser.Chars.whitespace;
 
 import org.petitparser.parser.Parser;
 import org.petitparser.tools.CompositeParser;
@@ -9,14 +10,14 @@ import org.petitparser.tools.Production;
 public class XmlParser extends CompositeParser {
 
   private static final String NAME_START_CHARS = ":A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD";
-  private static final String NAME_CHARS = NAME_START_CHARS + "-.0-9\u00B7\u0300-\u036F\u203F-\u2040";
+  private static final String NAME_CHARS = "-.0-9\u00B7\u0300-\u036F\u203F-\u2040" + NAME_START_CHARS;
 
   @Production Parser whitespaces;
   @Production Parser nameStartChar;
   @Production Parser nameChar;
 
   Parser whitespaces() {
-    return whitespaces().plus();
+    return whitespace().plus();
   }
 
   Parser nameStartChar() {
@@ -25,6 +26,10 @@ public class XmlParser extends CompositeParser {
 
   Parser nameChar() {
     return pattern(NAME_CHARS);
+  }
+
+  public static void main(String[] args) {
+    new XmlParser();
   }
 
 }
