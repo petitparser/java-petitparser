efae3d2cee73d9ba7b898b642980c17ac5259f6f
Lukas Renggli <renggli@gmail.com>
2012-04-08 09:47:14 +0200

    - optimize composite grammars - improved the javadoc of composite grammars

--- src/org/petitparser/tools/CompositeParser.java
+++ src/org/petitparser/tools/CompositeParser.java
@@ -8,21 +8,22 @@ import java.util.Map;
 import org.petitparser.parser.DelegateParser;
 import org.petitparser.parser.FailureParser;
 import org.petitparser.parser.Parser;
+import org.petitparser.utils.Transformations;
 
 import com.google.common.collect.Maps;
 
 /**
- * Helper to compose complex grammars from various primitive parsers.
- *
- * To create a new complex grammar subclass {@link CompositeParser}. For every
- * production create a method that returns its parser. Additionally every
- * production requires a field of the same name, same type and an annotation
- * {@link Production}, otherwise the production is not cached and cannot be
- * used in recursive grammars. Productions should refer to each other by reading
- * the respective fields.
+ * Helper to compose complex grammars from various primitive parsers. To create
+ * a new complex grammar subclass {@link CompositeParser}. For every production
+ * create a method that returns its parser. Additionally every production
+ * requires a field of the same name, same type and an annotation
+ * {@link Production}, otherwise the production is not cached and cannot be used
+ * in recursive grammars. Productions should refer to each other by reading the
+ * respective fields.
  *
  * <pre>
- * @Production Parser identifier;
+ * &#064;Production
+ * Parser identifier;
  *
  * Parser identifier() {
  *   return letter().seq(word().star()).flatten();
@@ -32,7 +33,7 @@ import com.google.common.collect.Maps;
  * The start production is returned from {@link #start()}.
  *
  * <pre>
- * @Override
+ * &#064;Override
  * Parser start() {
  *   return identifier.end();
  * }
@@ -43,16 +44,16 @@ import com.google.common.collect.Maps;
 public abstract class CompositeParser extends DelegateParser {
 
   /**
-   * Default constructor that triggers the magic to resolve recursive grammars.
+   * Default constructor that triggers the code building the composite grammar.
    */
   public CompositeParser() {
     super(new FailureParser("No parser defined"));
     initializeFields(getFieldsAndInitializers());
-    replace(getDelegate(), start());
+    replace(getDelegate(), Transformations.removeDelegates(start()));
   }
 
   /**
-   * Start production of the grammar.
+   * Returns the start production of the grammar.
    */
   protected abstract Parser start();
 
@@ -102,7 +103,8 @@ public abstract class CompositeParser extends DelegateParser {
       try {
         entry.getValue().setAccessible(true);
         Parser parser = (Parser) entry.getValue().invoke(this);
-        parsers.get(entry.getKey()).replace(parsers.get(entry.getKey()).getDelegate(), parser);
+        parsers.get(entry.getKey()).replace(
+            parsers.get(entry.getKey()).getDelegate(), parser);
         entry.getKey().set(this, parser);
       } catch (IllegalArgumentException exception) {
         exception.printStackTrace();
